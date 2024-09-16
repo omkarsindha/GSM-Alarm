527817e31988e3718a5499de18533ac1a1a4c129
Omkar <sindhao@sheridancollege.ca>
2024-09-16 16:44:24 -0400

    Added robust threading classes

--- LabMonitor.py
+++ LabMonitor.py
@@ -8,42 +8,59 @@ from utils.file_utils import write_history
 import threading
 
 
-class LabMonitor:
-    def __init__(self, check_interval=5):
-        self.history = "Config/past-alerts.json"
+class LabMonitor(threading.Thread):
+    def __init__(self, check_interval=5, debug=False, **kwargs):
+        super(LabMonitor, self).__init__(name="Monitor", **kwargs)
         self.config = Config(config_path="Config/config.json")
         self.sensor = TemperatureSensor()
-        self.sim7600x = SIM7600x()
         self.check_interval = check_interval
         self.last_msg_time = time.time() - self.config.report_interval
+        self.debug = debug
+        self.end_event = threading.Event()
+        self.sms_thread = SIM7600x(debug)
+        self.sms_thread.start()
         
-
-    def monitor_temperature(self):
-        while True:
+    
+    def run(self):
+        try:
+            self.monitor_loop()
+        except Exception as err:
+            error = str(err) if str(err) else str(err.__class__.__name__)
+            self.log("Thread failed: %s" % error)
+            self.error = "Unhandled exception: %s" % error
+    
+    
+    def monitor_loop(self):
+        while self.end_event.is_set() is False:
             temp = self.sensor.read_temp()
             current_time = time.time()
             if temp is not None:
-                print(f"Current Temperature: {temp}°C")
+                self.log(f"Current Temperature: {temp}°C")
                 if temp > self.config.max_temp and (current_time - self.last_msg_time > self.config.report_interval):   
                     self.last_msg_time = time.time()
-                    print("Sending alert messages...")
-                    self.sim7600x.send_sms_in_thread(self.config.numbers, self.config.message, temp)
-                    write_history(self.history, self.config.message, temp, current_time)
+                    self.log("Sending alert messages...")
+                    self.sms_thread.enqueue_sms(self.config.numbers, self.config.message, temp)
+                    write_history(self.config.message, temp, current_time)
             else:
-                print("Error reading temprature")
-                
+                self.log("Error reading temprature")   
             time.sleep(self.check_interval)
         
         
-    def start_monitoring_in_thread(self):
-        monitor_thread = threading.Thread(target=self.monitor_temperature)
-        monitor_thread.start()
+    def log(self, message):
+        if self.debug:
+            print(message)
+            
+    def stop(self, block=False):
+        """Signal the thread to end. Block only if block=True."""
+        self.end_event.set()
+        self.sms_thread.stop()
+        if block is True:
+            self.join()
+    
     
 if __name__ == "__main__":
-    monitor = LabMonitor()
-    try:
-        monitor.monitor_temperature()
-    except KeyboardInterrupt:
-        print("Monitoring stopped.")
-        
+    monitor = LabMonitor(debug=True)
+    monitor.start()
+    time.sleep(60)
+    monitor.stop()
         
--- coms/SIM7600x.py
+++ coms/SIM7600x.py
@@ -1,48 +1,120 @@
 import serial
 import time
 import threading
+import queue
 
 
-class SIM7600x:
-    def __init__(self):
-        self.port = "/dev/ttyAMA0"
+class SMSMessage(object):
+    def __init__(self, phone_number, message, temp):
+        self.phone_number = phone_number
+        self.message = message
+        self.temp = temp
         
-        
-    def send_at_command(self, ser, command, timeout=1):
-        ser.write((command + '\r\n').encode())
-        time.sleep(timeout)
-        response = ser.read(ser.in_waiting).decode()
-        return response
+    def __iter__(self):
+        return iter((self.phone_number, self.message, self.temp))
 
-    def send_sms(self, phone_number, message, temp):
+class SIM7600x(threading.Thread):
+    def __init__(self, port=serial.Serial("/dev/ttyAMA0", 115200, timeout=5), debug=False, **kwargs):
+        super(SIM7600x, self).__init__(name="SIM7600x", **kwargs)
+        self.ser = port
+        self.debug = debug
+        self.end_event = threading.Event()
+        self.sms_queue = queue.Queue() 
+        self.ser.write(("AT&F").encode())
+        
+    def run(self):
         try:
-            ser = serial.Serial(self.port, 115200, timeout=5)
-            self.send_at_command(ser, "AT")
-            self.send_at_command(ser, "AT+CMGF=1")  
-            self.send_at_command(ser, 'AT+CSCS="GSM"') 
-            self.send_at_command(ser, f'AT+CMGS="{phone_number}"')
+            self.sms_loop()
+        except Exception as err:
+            error = str(err) if str(err) else str(err.__class__.__name__)
+            self.log("Thread failed: %s" % error)
+            self.error = "Unhandled exception: %s" % error
+    
+    def enqueue_sms(self, numbers, message, temp):
+        for number in numbers:
+            message = SMSMessage(number, message, temp)
+            self.sms_queue.put(message)
+                 
+    def sms_loop(self):
+        while self.end_event.is_set() is False:
+            while self.sms_queue.qsize():
+                try:
+                    sms = self.sms_queue.get_nowait()
+                except queue.Empty:
+                    break
+                self.send_sms(sms)
+                
+    def send_sms(self, sms):
+        phone_number, message, temp = sms
+        try:
+            steps = [
+            ("AT&F", "OK"),
+            ("AT+CMGF=1", "OK"),
+            ('AT+CSCS="GSM"', "OK"),
+            (f'AT+CMGS="{phone_number}"', ">")
+            ]
+            
+            for cmd, exp in steps:
+                if not self.send_at_command(cmd, exp):
+                    self.log(f"Failed at step: {cmd}")
+                    return False
+                    
             rdble_time = time.strftime("%B %d %Y %H:%M:%S")
-            ser.write((f"{message}\n\nTemprature: {temp}°C\nTime: {rdble_time}" + '\x1A').encode())  # Send message and Ctrl+Z
+            msg = f"{message}\n\nTemprature: {temp} C \nTime: {rdble_time}"
+            
+            self.ser.write((msg + '\x1A').encode())
             time.sleep(1)
             
-            response = ser.read(ser.in_waiting).decode()
-            print(f"Final response: {response}")
-            
+            response = self.ser.read(self.ser.in_waiting).decode()
+            if "+CMGS:" in response:
+                self.log(f"Message sent successfully")
+                return True
+            else:
+                self.log(f"Failed to send message")
+                return False
+        
         except Exception as e:
-            print(f"Failed to send message: {e}")
-        finally:
-            if ser.is_open:
-                ser.close()
-         
+            self.log(f"Failed to send message: {e}")
+                            
+    def send_at_command(self, command, expected, timeout=1, retries=3):
+        self.log(f"Currently running Command: {command}")
+        for attempt in range(retries):
+            try:
+                self.ser.write((command + '\r\n').encode())
+                time.sleep(timeout)
                 
-    def send_sms_in_thread(self,numbers, message, temp):
-        def send_sms():
-            for number in numbers:
-                self.send_sms(number,message,temp)
-        sms_thread = threading.Thread(target=send_sms)
-        sms_thread.start()
+                response = self.ser.read(self.ser.in_waiting).decode()
+              
+                if expected in response:
+                    return True
+                else:
+                    self.log(f"Attempt {attempt + 1}: Expected '{expected}' but got '{response}'")
+            except Exception as e:
+                self.log(f"Attempt {attempt + 1} failed: {str(e)}")
+        return False
 
+    def factory_reset(self):
+        pass
+                
+    def log(self, message):
+        if self.debug:
+            print(message)
+    
+    def stop(self, block=False):
+        """Signal the thread to end. Block only if block=True."""
+        self.end_event.set()
+        if self.ser.is_open:
+            self.ser.close()
+            self.log("Serial POrt is now closed")
+        if block is True:
+            self.join() 
+                
    
-
+if __name__ == "__main__":
+    sms_thread = SIM7600x(debug=True)
+    sms_thread.start()
+    sms_thread.enqueue_sms(["+14374293006"], "Alert", "35")
+    sms_thread.stop()
+    
         
         
