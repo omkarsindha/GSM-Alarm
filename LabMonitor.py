589793298e2af3c10b24589fe67606f57ae4276d
omkarsindha <sindhao@sheridancollege.ca>
2024-10-08 22:46:28 -0400

    Improved Temperature Sensor module

--- LabMonitor.py
+++ LabMonitor.py
@@ -4,7 +4,7 @@ from SIM7600x import SIM7600x
 from UPS import UPS
 import time
 import os
-import json 
+import json
 import threading
 import schedule
 import traceback
@@ -18,20 +18,20 @@ class LabMonitor(threading.Thread):
     """
     def __init__(self, check_interval=2, debug=False, **kwargs):
         super(LabMonitor, self).__init__(name="Monitor", **kwargs)
-        
+
         # Initialize configuration and components
         self.config = Config(config_path="Config/config.json")
         self.sensor = TemperatureSensor()
         self.ups = UPS()
         self.sms_thread = SIM7600x(parent=self, debug=debug)
         self.sms_thread.start()
-        
+
         self.debug = debug
         self.check_interval = check_interval
         self.last_msg_time = time.time() - self.config.alert_interval
         self.end_event = threading.Event()
-        
-        
+
+
         self.alert_sent = False
         self.power: str = "120V-AC"    # Initial power source is 120V-AC
         self.power_last_state: str = "120V-AC"
@@ -41,7 +41,7 @@ class LabMonitor(threading.Thread):
 
         self.schedule_daily_status()
         self.log("Initiated an instance of monitor thread")
-         
+
     def run(self):
         """
         Main method that runs when the thread starts.
@@ -53,46 +53,43 @@ class LabMonitor(threading.Thread):
             error = str(err) if str(err) else str(err.__class__.__name__)
             self.log("Thread failed: %s" % error)
             self.log(traceback.format_exc())
-            
+
     def monitor_loop(self):
         """Main logic for Lab Monitor"""
         while not self.end_event.is_set():
             # Read current temperature and power status
             self.readings = self.sensor.read_temp()  # List of readings in the form of [{"sys/bus/w1slave":"xx"}, ...]
-            self.power = self.ups.get_status()  # 120V-AC or UPS
-            
-            if None in self.readings or self.power is None:
-                continue
+            # Get UPS power status either '120V-AC', 'UPS', or 'Error'.
+            self.power = self.ups.get_status()
 
             self.log(f"Status (Temperature Readings: {self.readings} || Power: {self.power})")
 
             if self.config.armed:  # If the Alarm is active
                 cur_time = time.time()
-                
+
                 # Process each sensors reading to check if the temperature is above the trigger threshold
                 for reading in self.readings:
+                    # The key is the sensor filename and value is degrees C.
                     sensor_path, temperature = list(reading.items())[0]  # Unpack the dict
-                    temperature = temperature
                     if sensor_path in self.config.sensors:
                         trigger = self.config.sensors[sensor_path]['trigger']
                         if temperature > trigger:
                             self.sensors_above_threshold[sensor_path] = {
                                 "temperature": temperature,
                                 "name": self.config.sensors[sensor_path]['name']
-                            }  
+                            }
 
                 # Check if temperature has returned to normal
                 for reading in self.readings:
                     sensor_path, temperature = list(reading.items())[0]
-                    temperature = temperature
                     if sensor_path in self.sensors_above_threshold:
                         trigger = self.config.sensors[sensor_path]['trigger']
                         if temperature < trigger - self.config.hysteresis:
                             self.log(f"{self.sensors_above_threshold[sensor_path].get('name')} is back to normal temperature")
                             del self.sensors_above_threshold[sensor_path]                     # Delete it from the list
-                            
-                            if not self.sensors_above_threshold:     # Only if sensor list gets empty send back to normal message    
-                                self.log("Sending temperature back to normal message")       
+
+                            if not self.sensors_above_threshold:     # Only if sensor list gets empty send back to normal message
+                                self.log("Sending temperature back to normal message")
                                 self.alert_sent = False
                                 msg = f"Alert Resolved\n\nTemperature is back to normal :)\n\nLocation: {self.config.location} \nTime: {get_rdbl_time()}"
                                 self.sms_thread.enqueue_sms(self.config.numbers, msg)
@@ -111,10 +108,10 @@ class LabMonitor(threading.Thread):
                             [f"{info['name']}: {info['temperature']}°C" for info in self.sensors_above_threshold.values()]
                         )
                         msg = f"Alert\n\nSensors Above threshold:\n{sensor_details}\n\nLocation: {self.config.location}\nTime: {get_rdbl_time()}"
-                        
+
                         self.sms_thread.enqueue_sms(self.config.numbers, msg)
                         write_history("High temperature")
-                    
+
                 # Handle changes in power source
                 if self.power != self.power_last_state:
                     self.log("Sending power type changed message")
@@ -126,9 +123,10 @@ class LabMonitor(threading.Thread):
                     self.power_last_state = self.power
                     self.sms_thread.enqueue_sms(self.config.numbers, msg)
                     write_history(f"Power changed to {self.power}")
-                    
-                # Case when battery is low 
+
+                # Case when battery is low
                 if self.power == "UPS":
+                    # NOTE: get_battery_level might return a string which will throw an exception if compared to an integer.
                     percentage = self.ups.get_battery_level()
                     if percentage <= 80 and self.low_battery == False:
                         self.log("Sending low battery message")
@@ -136,11 +134,11 @@ class LabMonitor(threading.Thread):
                         self.sms_thread.enqueue_sms(self.config.numbers, msg)
                         write_history("Low battery")
                         self.low_battery = True
-                    
+
                 # Run any scheduled daily status reports
                 schedule.run_pending()
             time.sleep(self.check_interval)
-            
+
     def stop(self, block=False):
         """
         Signal the thread to end. Block only if block=True.
@@ -149,16 +147,16 @@ class LabMonitor(threading.Thread):
         self.sms_thread.stop()
         if block:
             self.join()
-            
-    def get_config(self):  
+
+    def get_config(self):
         """
         Get all the information from Config, SIM7600x and Monitor
         """
         network_type = self.sms_thread.network_type
         signal_strength = self.sms_thread.signal_strength
-        
+
         # Config might have old sensors so filtering it to get only the ones available in the latest readings
-        sensor_files = {sensor_path for reading in self.readings for sensor_path in reading.keys()}                                
+        sensor_files = {sensor_path for reading in self.readings for sensor_path in reading.keys()}
         filtered_sensors = {sensor_path: info for sensor_path, info in self.config.sensors.items() if sensor_path in sensor_files}
 
         # Initialize the intersection list
@@ -174,25 +172,25 @@ class LabMonitor(threading.Thread):
                     "temperature": temperature
                 }
                 intersection.append(combined_info)
-            
+
         return {
-                "high_temperature": bool(self.sensors_above_threshold), 
+                "high_temperature": bool(self.sensors_above_threshold),
                 "location": self.config.location,
-                "hys": self.config.hysteresis, 
-                "interval": self.config.alert_interval / 60, 
+                "hys": self.config.hysteresis,
+                "interval": self.config.alert_interval / 60,
                 "daily_report_time": self.config.daily_report_time,
                 "armed": self.config.armed,
                 "send_daily_report": self.config.send_daily_report,
                 "repeat_alerts": self.config.repeat_alerts,
-                "signal_strength": signal_strength, 
-                "signal_type": network_type, 
-                "pi_time": time.strftime("%B %d  %H:%M"),  
+                "signal_strength": signal_strength,
+                "signal_type": network_type,
+                "pi_time": time.strftime("%B %d  %H:%M"),
                 "numbers": self.config.numbers_list,
                 "power": self.power,
                 "battery": self.ups.get_battery_level(),
                 "sensors": intersection
                 }
-    
+
     def schedule_daily_status(self):
         """
         Schedules the daily report SMS
@@ -211,14 +209,14 @@ class LabMonitor(threading.Thread):
             msg = f"Daily Report\n\nLocation: {self.config.location}\nTime: {get_rdbl_time()}"
             self.sms_thread.enqueue_sms(self.config.daily_numbers, msg)
             write_history("Daily report")
-            
+
     def log(self, message):
         """
         Logs a message if debug mode is enabled
         """
         if self.debug:
             print(message)
-            
+
     def handle_sms(self, sms):
         """
         Replies to SMS by the users of the Monitor after Authenticating and Authorizing
@@ -230,7 +228,7 @@ class LabMonitor(threading.Thread):
             for contact in self.config.numbers_list:        # Getting the name corresponding to the number
                  if contact['number'] == num:
                     name = contact['name']
-        
+
             if text == 'status':  # Same for admin and normal users
                 config = self.get_config()
                 armed = 'Armed' if config['armed'] else 'Disarmed'
@@ -260,32 +258,32 @@ class LabMonitor(threading.Thread):
                     if parts[0] == 'set' and parts[1] == 'repeat-alerts' and parts[2] in ['on', 'off']:
                         status = parts[2].lower()
                         if status == 'on':
-                            update_config(repeat_alerts = True)  
+                            update_config(repeat_alerts = True)
                         else:
-                            update_config(repeat_alerts = False)  
+                            update_config(repeat_alerts = False)
                         self.sms_thread.enqueue_sms([num], f"Repeat-Alerts set to '{status}'.")
                         write_history(f"Repeat-Alerts set to '{status}' by {name}")
-                        
+
                     elif parts[0] == 'set' and parts[1] == 'alert-interval' and is_positive_number(parts[2]):
                         interval = int(parts[2])
-                        update_config(interval = interval) 
+                        update_config(interval = interval)
                         self.sms_thread.enqueue_sms([num], f"Alert interval set to {interval} minutes")
-                        write_history(f"Alert interval set to {interval} minutes by {name}")                       
-                        
+                        write_history(f"Alert interval set to {interval} minutes by {name}")
+
                     else:
                         self.sms_thread.enqueue_sms([num], "Invalid value for repeat-alerts. Please enter 'on' or 'off'.")
                         write_history(f"Invalid command sent by {name}")
-                    return 
+                    return
 
             self.sms_thread.enqueue_sms([num], "Invalid command. Send 'help' for more details.")
             write_history(f"Invalid command sent by {name}")
 
-            
-           
-        
+
+
+
 if __name__ == "__main__":
     # Create and start a LabMonitor instance for testing
     monitor = LabMonitor(debug=True)
     monitor.start()
-    time.sleep(60)  
+    time.sleep(60)
     monitor.stop()
--- UPS.py
+++ UPS.py
@@ -1,11 +1,11 @@
-from pijuice import PiJuice
+import pijuice
 import threading
-import time 
+import time
 
 class UPS():
     def __init__(self):
-      self.ups = PiJuice(1, 0x14)
-                 
+      self.ups = pijuice.PiJuice(1, 0x14)
+
     def get_status(self) -> str:
         """Returns '120V-AC' or 'UPS' string depending on the state of the power supply to pi"""
         status = self.ups.status.GetStatus().get('data', None)
@@ -15,16 +15,16 @@ class UPS():
             else:
                 return "120V-AC"
         else:
-            return None
-        
+            return "Error"
+
     def get_battery_level(self) -> int:
         """Returns the battery percentage of the UPS"""
-        status = self.ups.status.GetChargeLevel()  
+        status = self.ups.status.GetChargeLevel()
         if status['error'] == 'NO_ERROR':
-            return status['data']  
+            return status['data']
         else:
             return f"Error: {status['error']}"
-                       
+
 if __name__ == "__main__":
     ups = UPS()
     for _ in range(5):
@@ -32,8 +32,8 @@ if __name__ == "__main__":
         print(type(ups.get_battery_level()))
         time.sleep(1)
     print(ups.get_battery_level())
-        
-        
+
+
 # class UPS(threading.Thread):
 #     def __init__(self, debug=False, **kwargs):
 #         super(UPS, self).__init__(name="UPS", **kwargs)
@@ -42,11 +42,11 @@ if __name__ == "__main__":
 #         self.debug = debug
 #         self.end_event = threading.Event()
 #         self.lock = threading.Lock()
-                 
+
 #     def get_power(self):
 #         with self.lock:
 #             return self._power
-        
+
 #     def ups_loop(self):
 #         while self.end_event.is_set() is False:
 #             status = self.ups.status.GetStatus()['data']
@@ -57,7 +57,7 @@ if __name__ == "__main__":
 #                 with self.lock:
 #                     self._power = "GRID"
 #             time.sleep(1)
-            
+
 #     def run(self):
 #         try:
 #             self.ups_loop()
@@ -65,24 +65,23 @@ if __name__ == "__main__":
 #             error = str(err) if str(err) else str(err.__class__.__name__)
 #             self.log("Thread failed: %s" % error)
 #             self.error = "Unhandled exception: %s" % error
-            
+
 #     def stop(self, block=False):
 #         """Signal the thread to end. Block only if block=True."""
 #         self.end_event.set()
 #         if block is True:
-#             self.join() 
-            
+#             self.join()
+
 #     def log(self, message):
 #         if self.debug:
 #             print(message)
-            
-            
+
+
 # if __name__ == "__main__":
 #     ups_thread = UPS(debug=True)
 #     ups_thread.start()
 #     time.sleep(5)
 #     ups_thread.stop()
 #     print(f"{ups_thread.get_power()} hi")
-    
-        
-        
+
+
